e8ecfc976ebc9a88584c714509a916fae7443fe2
xborismenx <boris.tikhonov.21@gmail.com>
2024-08-29 19:47:25 +0300

    created serializers for models

---
+++ airport/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+
+from airport.models import Airport, Route, Airplane, AirplaneType, Order, Ticket, Flight, Crew
+
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = ("name", "closest_big_city")
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = ("source", "destination", "distance")
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AirplaneType
+        fields = ("name",)
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airplane
+        fields = ("name", "rows", "seats_in_row", "airplane_type")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ("created_at", "user")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat", "flight", "order",)
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = ("route", "airplane", "departure_time", "arrival_time")
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = ("first_name", "last_name", "flight")
