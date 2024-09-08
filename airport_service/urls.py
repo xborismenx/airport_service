ca865eff4544b6f6047604d71c80ef7ba75edc91
xborismenx <boris.tikhonov.21@gmail.com>
2024-09-08 20:02:17 +0300

    add API schema and documentation routes with Swagger and Redoc & add urls for debug toolbar

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("airport/", include("airport.urls")),
-]
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+] + debug_toolbar_urls()
