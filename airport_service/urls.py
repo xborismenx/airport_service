ffa23c2750d5e8473fb795abcd1e5d0d700334d5
xborismenx <boris.tikhonov.21@gmail.com>
2024-09-09 19:12:08 +0300

    added path for user.urls

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -22,6 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("airport/", include("airport.urls")),
+    path("user/", include("user.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
