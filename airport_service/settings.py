269cecbcb78777c3ba43ac322a80a1626d649925
xborismenx <boris.tikhonov.21@gmail.com>
2024-09-18 00:58:50 +0300

    defined MEDIA_ROOT & MEDIA_URL

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -147,6 +147,10 @@ AUTH_USER_MODEL = "user.User"
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
