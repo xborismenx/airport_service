00d963471af2ec3f0045877bcb411e972a159abc
xborismenx <boris.tikhonov.21@gmail.com>
2024-09-18 19:15:27 +0300

    added in User email field

--- user/models.py
+++ user/models.py
@@ -3,4 +3,10 @@ from django.db import models
 
 
 class User(AbstractUser):
-    ...
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
